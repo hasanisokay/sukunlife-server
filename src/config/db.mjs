b9aa2bd73d6053b897a3c5d1f41f2b2a5d8debdb
hasanisokay <hasanisokay@gmail.com>
2025-02-25 15:53:11 +0600

    chnaged db name

--- src/config/db.mjs
+++ src/config/db.mjs
@@ -13,7 +13,7 @@ const dbConnect = async () => {
         deprecationErrors: true,
       },
     });
-    db = client.db("sukunlife");
+    db = client.db("sukunlife-big");
     await client.db("admin").command({ ping: 1 });
     return db;
   } catch (e) {
