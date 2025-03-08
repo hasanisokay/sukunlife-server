7fe63f2c7e9e9789a39550312c578d671096bf93
hasanisokay <hasanisokay@gmail.com>
2025-03-08 12:56:09 +0600

    chnaged domain

--- src/constants/names.mjs
+++ src/constants/names.mjs
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-
+const isProduction = process.env.NODE_ENV === "production";
 export const ACCESS_TOKEN_SECRET_KEY = process.env.JWT_SECRET;
 export const REFRESH_SECRET_KEY = process.env.REFRESH_JWT_SECRET;
 
@@ -10,4 +10,4 @@ export const ACCESS_EXPIRATION = "24h"; // Shorter expiration for access token
 export const REFRESH_EXPIRATION = "30d"; // Longer expiration for refresh token
 export const ACCESS_COOKIE_MAX_AGE = 24 * 60 * 60 * 1000; //10 hours
 export const REFRESH_COOKIE_MAX_AGE = 30 * 24 * 60 * 60 * 1000;
-export const DOMAIN = '.vercel.app'
+export const DOMAIN = isProduction ? '.vercel.app' :''
