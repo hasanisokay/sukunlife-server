5128181c15b532afee2eac03ca29da418bc021f0
hasanisokay <hasanisokay@gmail.com>
2026-01-24 15:53:13 +0600

    added upload to allowed origin

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ const corsOptions = {
       "http://localhost:3000",
       "https://sukunlife.com",
       "https://www.sukunlife.com",
+      "https://upload.sukunlife.com"
     ];
 
     if (!origin || allowedOrigins.includes(origin)) {
